6632ae7f4e1c85cb684306e551c3b27d57d4d388
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-11-04 21:43:23 +0200

    add simpleLightBox adn complete task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCardSet = galleryItems.map(createGalleryCard).join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryCardSet);
+
+function createGalleryCard({ preview, original, description }) {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
