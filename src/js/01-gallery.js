5616269c40f7dac18297eeb3cbd3f852302b7155
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-11-18 01:04:17 +0200

    refact code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
 const galleryRef = document.querySelector('.gallery');
 const galleryCardSet = galleryItems.map(createGalleryCard).join('');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,37 +9,40 @@ const refs = {
 refs.form.addEventListener('submit', onFormSumbit);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
-const STORAGE_EMAIL_KEY = 'email';
-const STORAGE_MESSAGE_KEY = 'message';
+const STORAGE_KEY = 'formData';
+const formData = {};
 
 getLocalStorageItems();
 
 function onFormSumbit(e) {
   e.preventDefault();
+
   consoleFormData(e.currentTarget);
   e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_EMAIL_KEY);
-  localStorage.removeItem(STORAGE_MESSAGE_KEY);
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onInput(e) {
-  if (e.target.name === 'email')
-    localStorage.setItem(STORAGE_EMAIL_KEY, e.target.value);
-  if (e.target.name === 'message')
-    localStorage.setItem(STORAGE_MESSAGE_KEY, e.target.value);
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('formData', JSON.stringify(formData));
 }
 
-function getLocalStorageItems(props) {
-  if (localStorage.getItem(STORAGE_EMAIL_KEY)) {
-    refs.email.value = localStorage.getItem(STORAGE_EMAIL_KEY);
-  }
-  if (localStorage.getItem(STORAGE_MESSAGE_KEY)) {
-    refs.message.value = localStorage.getItem(STORAGE_MESSAGE_KEY);
-  }
+function getLocalStorageItems() {
+  const storageItem = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log('storageItem', storageItem);
+
+  if (!storageItem) return;
+
+  Object.keys(storageItem).forEach(key => {
+    const element = refs.form.querySelector(`[name="${key}"]`);
+    element.value = storageItem[key];
+  });
 }
 
 function consoleFormData(form) {
   const feedbackData = {};
+
   new FormData(form).forEach((value, key) => (feedbackData[key] = value));
   console.log('feedbackData', feedbackData);
 }
